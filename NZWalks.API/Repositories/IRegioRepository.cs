ee75e66fc8bcb4904b9dd8e25acb6d3f38bfd181
Sphesihle Jamile <Sphesihlej@geotab.co.za>
2023-01-12 00:10:26 +0200

    Phase 1 Complete

---
+++ NZWalks.API/Repositories/IRegioRepository.cs
@@ -0,0 +1,13 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegioRepository
+    {
+        Task<IEnumerable<Region>> GetAllAsync();
+        Task<Region> GetAsync(Guid id);
+        Task<Region> AddAsync(Region region);
+        Task<Region> UpdateAsync(Guid id, Region region);
+        Task<Region> DeleteAsync(Guid id);
+    }
+}
